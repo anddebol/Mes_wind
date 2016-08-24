69b3b4a51f996acdb252d6af7c5863b089932009
Anton Kudryashov <qubabox@mail.ru>
2016-08-25 01:11:13 +0300

    project reverted by VS10

---
+++ Backup/MES_Wind/frmMain.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace MES_Wind
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.appManager1 = new DotSpatial.Controls.AppManager();
+            this.sdmMapLegend = new DotSpatial.Controls.SpatialDockManager();
+            this.legend1 = new DotSpatial.Controls.Legend();
+            this.map1 = new DotSpatial.Controls.Map();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sdmDataOperations = new DotSpatial.Controls.SpatialDockManager();
+            this.gbOperations = new System.Windows.Forms.GroupBox();
+            this.btnViewAttributeTable = new System.Windows.Forms.Button();
+            this.dgvAttributeTable = new System.Windows.Forms.DataGridView();
+            this.bntLoadWindX = new System.Windows.Forms.Button();
+            this.btnLoadWindY = new System.Windows.Forms.Button();
+            this.btnCalcStress = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sdmMapLegend)).BeginInit();
+            this.sdmMapLegend.Panel1.SuspendLayout();
+            this.sdmMapLegend.Panel2.SuspendLayout();
+            this.sdmMapLegend.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sdmDataOperations)).BeginInit();
+            this.sdmDataOperations.Panel1.SuspendLayout();
+            this.sdmDataOperations.Panel2.SuspendLayout();
+            this.sdmDataOperations.SuspendLayout();
+            this.gbOperations.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttributeTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // appManager1
+            // 
+            this.appManager1.Directories = ((System.Collections.Generic.List<string>)(resources.GetObject("appManager1.Directories")));
+            this.appManager1.DockManager = this.sdmMapLegend;
+            this.appManager1.HeaderControl = null;
+            this.appManager1.Legend = this.legend1;
+            this.appManager1.Map = this.map1;
+            this.appManager1.ProgressHandler = null;
+            this.appManager1.ShowExtensionsDialogMode = DotSpatial.Controls.ShowExtensionsDialogMode.Default;
+            // 
+            // sdmMapLegend
+            // 
+            this.sdmMapLegend.Dock = System.Windows.Forms.DockStyle.Top;
+            this.sdmMapLegend.Location = new System.Drawing.Point(0, 0);
+            this.sdmMapLegend.Name = "sdmMapLegend";
+            // 
+            // sdmMapLegend.Panel1
+            // 
+            this.sdmMapLegend.Panel1.Controls.Add(this.legend1);
+            // 
+            // sdmMapLegend.Panel2
+            // 
+            this.sdmMapLegend.Panel2.Controls.Add(this.map1);
+            this.sdmMapLegend.Size = new System.Drawing.Size(782, 234);
+            this.sdmMapLegend.SplitterDistance = 260;
+            this.sdmMapLegend.TabControl1 = null;
+            this.sdmMapLegend.TabControl2 = null;
+            this.sdmMapLegend.TabIndex = 0;
+            // 
+            // legend1
+            // 
+            this.legend1.BackColor = System.Drawing.Color.White;
+            this.legend1.ControlRectangle = new System.Drawing.Rectangle(0, 0, 260, 234);
+            this.legend1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.legend1.DocumentRectangle = new System.Drawing.Rectangle(0, 0, 187, 428);
+            this.legend1.HorizontalScrollEnabled = true;
+            this.legend1.Indentation = 30;
+            this.legend1.IsInitialized = false;
+            this.legend1.Location = new System.Drawing.Point(0, 0);
+            this.legend1.MinimumSize = new System.Drawing.Size(5, 5);
+            this.legend1.Name = "legend1";
+            this.legend1.ProgressHandler = null;
+            this.legend1.ResetOnResize = false;
+            this.legend1.SelectionFontColor = System.Drawing.Color.Black;
+            this.legend1.SelectionHighlight = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(238)))), ((int)(((byte)(252)))));
+            this.legend1.Size = new System.Drawing.Size(260, 234);
+            this.legend1.TabIndex = 0;
+            this.legend1.TabStop = false;
+            this.legend1.Text = "legend1";
+            this.legend1.VerticalScrollEnabled = true;
+            // 
+            // map1
+            // 
+            this.map1.AllowDrop = true;
+            this.map1.BackColor = System.Drawing.Color.White;
+            this.map1.CollectAfterDraw = false;
+            this.map1.CollisionDetection = false;
+            this.map1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.map1.ExtendBuffer = false;
+            this.map1.FunctionMode = DotSpatial.Controls.FunctionMode.Pan;
+            this.map1.IsBusy = false;
+            this.map1.IsZoomedToMaxExtent = false;
+            this.map1.Legend = this.legend1;
+            this.map1.Location = new System.Drawing.Point(0, 0);
+            this.map1.Name = "map1";
+            this.map1.ProgressHandler = null;
+            this.map1.ProjectionModeDefine = DotSpatial.Controls.ActionMode.Prompt;
+            this.map1.ProjectionModeReproject = DotSpatial.Controls.ActionMode.Prompt;
+            this.map1.RedrawLayersWhileResizing = false;
+            this.map1.SelectionEnabled = true;
+            this.map1.Size = new System.Drawing.Size(518, 234);
+            this.map1.TabIndex = 0;
+            this.map1.ZoomOutFartherThanMaxExtent = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(114, 360);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.sdmDataOperations);
+            this.panel1.Controls.Add(this.sdmMapLegend);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(114, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(782, 360);
+            this.panel1.TabIndex = 1;
+            // 
+            // sdmDataOperations
+            // 
+            this.sdmDataOperations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sdmDataOperations.Location = new System.Drawing.Point(0, 234);
+            this.sdmDataOperations.Name = "sdmDataOperations";
+            this.sdmDataOperations.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // sdmDataOperations.Panel1
+            // 
+            this.sdmDataOperations.Panel1.Controls.Add(this.gbOperations);
+            // 
+            // sdmDataOperations.Panel2
+            // 
+            this.sdmDataOperations.Panel2.Controls.Add(this.dgvAttributeTable);
+            this.sdmDataOperations.Size = new System.Drawing.Size(782, 126);
+            this.sdmDataOperations.SplitterDistance = 55;
+            this.sdmDataOperations.TabControl1 = null;
+            this.sdmDataOperations.TabControl2 = null;
+            this.sdmDataOperations.TabIndex = 1;
+            // 
+            // gbOperations
+            // 
+            this.gbOperations.Controls.Add(this.btnViewAttributeTable);
+            this.gbOperations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbOperations.Location = new System.Drawing.Point(0, 0);
+            this.gbOperations.Name = "gbOperations";
+            this.gbOperations.Size = new System.Drawing.Size(782, 55);
+            this.gbOperations.TabIndex = 0;
+            this.gbOperations.TabStop = false;
+            this.gbOperations.Text = "Operations";
+            // 
+            // btnViewAttributeTable
+            // 
+            this.btnViewAttributeTable.Location = new System.Drawing.Point(7, 22);
+            this.btnViewAttributeTable.Name = "btnViewAttributeTable";
+            this.btnViewAttributeTable.Size = new System.Drawing.Size(188, 23);
+            this.btnViewAttributeTable.TabIndex = 0;
+            this.btnViewAttributeTable.Text = "View Attribute table";
+            this.btnViewAttributeTable.UseVisualStyleBackColor = true;
+            this.btnViewAttributeTable.Click += new System.EventHandler(this.btnViewAttributeTable_Click);
+            // 
+            // dgvAttributeTable
+            // 
+            this.dgvAttributeTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAttributeTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAttributeTable.Location = new System.Drawing.Point(0, 0);
+            this.dgvAttributeTable.Name = "dgvAttributeTable";
+            this.dgvAttributeTable.RowTemplate.Height = 24;
+            this.dgvAttributeTable.Size = new System.Drawing.Size(782, 67);
+            this.dgvAttributeTable.TabIndex = 0;
+            // 
+            // bntLoadWindX
+            // 
+            this.bntLoadWindX.Location = new System.Drawing.Point(12, 74);
+            this.bntLoadWindX.Name = "bntLoadWindX";
+            this.bntLoadWindX.Size = new System.Drawing.Size(96, 46);
+            this.bntLoadWindX.TabIndex = 2;
+            this.bntLoadWindX.Text = "Load Xwind";
+            this.bntLoadWindX.UseVisualStyleBackColor = true;
+            this.bntLoadWindX.Click += new System.EventHandler(this.bntLoadWindX_Click);
+            // 
+            // btnLoadWindY
+            // 
+            this.btnLoadWindY.Location = new System.Drawing.Point(12, 147);
+            this.btnLoadWindY.Name = "btnLoadWindY";
+            this.btnLoadWindY.Size = new System.Drawing.Size(96, 46);
+            this.btnLoadWindY.TabIndex = 3;
+            this.btnLoadWindY.Text = "Load Ywind";
+            this.btnLoadWindY.UseVisualStyleBackColor = true;
+            this.btnLoadWindY.Click += new System.EventHandler(this.btnLoadWindY_Click);
+            // 
+            // btnCalcStress
+            // 
+            this.btnCalcStress.Location = new System.Drawing.Point(12, 209);
+            this.btnCalcStress.Name = "btnCalcStress";
+            this.btnCalcStress.Size = new System.Drawing.Size(96, 42);
+            this.btnCalcStress.TabIndex = 4;
+            this.btnCalcStress.Text = "Calculate Wind Stress";
+            this.btnCalcStress.UseVisualStyleBackColor = true;
+            this.btnCalcStress.Click += new System.EventHandler(this.btnCalcStress_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 360);
+            this.Controls.Add(this.btnCalcStress);
+            this.Controls.Add(this.btnLoadWindY);
+            this.Controls.Add(this.bntLoadWindX);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitter1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.sdmMapLegend.Panel1.ResumeLayout(false);
+            this.sdmMapLegend.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sdmMapLegend)).EndInit();
+            this.sdmMapLegend.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.sdmDataOperations.Panel1.ResumeLayout(false);
+            this.sdmDataOperations.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sdmDataOperations)).EndInit();
+            this.sdmDataOperations.ResumeLayout(false);
+            this.gbOperations.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttributeTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DotSpatial.Controls.AppManager appManager1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bntLoadWindX;
+        private DotSpatial.Controls.SpatialDockManager sdmMapLegend;
+        private DotSpatial.Controls.Legend legend1;
+        private DotSpatial.Controls.Map map1;
+        private System.Windows.Forms.Button btnLoadWindY;
+        private System.Windows.Forms.Button btnCalcStress;
+        private DotSpatial.Controls.SpatialDockManager sdmDataOperations;
+        private System.Windows.Forms.GroupBox gbOperations;
+        private System.Windows.Forms.DataGridView dgvAttributeTable;
+        private System.Windows.Forms.Button btnViewAttributeTable;
+    }
+}
---
+++ Backup/MES_Wind/frmMain.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel.Composition;
+using DotSpatial.Controls;
+using DotSpatial.Topology;
+using DotSpatial.Serialization;
+using DotSpatial.Data;
+
+
+
+namespace MES_Wind
+{
+    public partial class frmMain : Form
+    {
+       
+        [Export("Shell", typeof(ContainerControl))]
+        private static ContainerControl Shell;
+        #region "Control variables"
+         double distThreshold = 500;
+        #endregion
+
+        public frmMain()
+        {
+
+            InitializeComponent();
+            if (DesignMode) return;
+            Shell = this;
+            appManager1.LoadExtensions();
+        }
+        #region
+        /// <summary>
+        /// This function is used to check if the segment of powerline is broken. 
+        /// Based on the given line segment's start and endpoint, it divides the line on
+        /// suitable subsegments defined by threshold length - dThreshold
+        /// </summary>
+        /// <param name="startX">Line segement's start X point</param>
+        /// <param name="startY">Line segement's start Y point</param>
+        /// <param name="endX">Line segement's end X point</param>
+        /// <param name="endY">Line segement's end Y point</param>
+        /// <param name="dThreshold">Line segment length threshold</param>
+        /// <param name="Uwind_raster">Raster Layer for progmostic wind X coord</param>
+        /// <param name="Vwind_raster">Raster Layer for progmostic wind Y coord</param>
+        /// <returns>List of booleans with coordinates if any of them are true, line is broken</returns>
+        /// <remarks></remarks>
+#endregion
+        public List<CheckPoint> CalcBrkPoint(double startX, double startY, double endX, double endY, double dThreshold, IMapRasterLayer Uwind_raster, IMapRasterLayer Vwind_raster)
+        {
+            List<CheckPoint> lineCheckPoint = new List<CheckPoint>();
+            double uwind = 0;
+            double vwind = 0;
+            double umod = 0;
+            double anglewind = 0;
+            double distance = Math.Sqrt((endX - startX) * (endX - startX) + (endY - startY) * (endY - startY));
+            double distpropD = distance / dThreshold;
+            int distpropI = Convert.ToInt32(distpropD);
+            double curX = startX;
+            double curY = startY;
+            CheckPoint chkpnt = new CheckPoint();
+            if (distpropI > 1)
+            {
+                double constXdiff = (endX - startX) / distpropI;
+                double constYdiff = (endY - startY) / distpropI;
+                for (int j = 1; j < distpropI + 1; j++)
+                {
+                    if (j == 1)
+                    {
+                        curX = startX + constXdiff / 2;
+                        curY = startY + constXdiff / 2; 
+                    }
+                    else
+                    {
+                        curX = curX + constXdiff;
+                        curY = curY + constYdiff;
+                    }
+                    Coordinate coords = new Coordinate(curX,curY);
+                    RcIndex rowColumnU = Uwind_raster.DataSet.Bounds.ProjToCell(coords);
+                    uwind = Uwind_raster.DataSet.Value[rowColumnU.Row, rowColumnU.Column];
+                    RcIndex rowColumnV = Vwind_raster.DataSet.Bounds.ProjToCell(coords);
+                    vwind = Vwind_raster.DataSet.Value[rowColumnV.Row, rowColumnV.Column];
+                    umod = Math.Sqrt(uwind*uwind + vwind*vwind);
+                    anglewind = Math.Atan2(uwind,vwind);
+                    chkpnt.X = curX;
+                    chkpnt.Y = curY;
+                    chkpnt.Ifbroken = false;
+                    lineCheckPoint.Add(chkpnt);
+                }
+            }
+            return lineCheckPoint;
+        }
+
+
+        private void bntLoadWindX_Click(object sender, EventArgs e)
+        {
+            map1.AddLayer();
+            map1.ZoomToMaxExtent();
+
+        }
+
+        private void btnLoadWindY_Click(object sender, EventArgs e)
+        {
+            map1.AddLayer();
+            map1.ZoomToMaxExtent();
+        }
+
+        private void btnViewAttributeTable_Click(object sender, EventArgs e)
+        {
+            //Declare a datatable
+            DataTable dt = null;
+
+            if (map1.GetLineLayers().Count() > 0)
+            {
+                MapLineLayer pwlLayer = default(MapLineLayer);
+
+                pwlLayer = (MapLineLayer)map1.GetLineLayers()[0];
+
+                if (pwlLayer == null)
+                {
+                    MessageBox.Show("The layer is not a line layer.");
+                }
+                else
+                {
+                    //Get the shapefile's attribute table to our datatable dt
+                    dt = pwlLayer.DataSet.DataTable;
+
+                    //Set the datagridview datasource from datatable dt
+                    dgvAttributeTable.DataSource = dt;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please add a layer to the map.");
+            }
+
+        
+
+    }
+
+        private void btnCalcStress_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //extract prognostic u layer
+                IMapRasterLayer u_rasterLayer = default(IMapRasterLayer);
+                IMapRasterLayer v_rasterLayer = default(IMapRasterLayer);
+                if (map1.GetRasterLayers().Count() == 1)
+                {
+                    MessageBox.Show("Please add a raster layer");
+                    return;
+                }
+
+                //use the first raster layer in the map
+                u_rasterLayer = map1.GetRasterLayers()[0];
+                v_rasterLayer = map1.GetRasterLayers()[1];
+
+                //get the powerline  line layer
+                IMapLineLayer pwlLayer = default(IMapLineLayer);
+                if (map1.GetLineLayers().Count() == 0)
+                {
+                    MessageBox.Show("Please add powerline shapefile");
+                    return;
+                }
+                pwlLayer = map1.GetLineLayers()[0];
+                //copy line layer FeatureSet
+                IFeatureSet pwlineSet = pwlLayer.DataSet;
+                // new FeatureSet for resulting broken powerlines
+                IFeatureSet brklineSet = new FeatureSet(FeatureType.Line);
+                DataTable dt = pwlineSet.DataTable;
+                List<CheckPoint> fullCheckList = new List<CheckPoint>();
+                foreach (IFeature feature in pwlineSet.Features)
+                {
+                    List<CheckPoint> lineCheckList = new List<CheckPoint>();
+                    LineString linestr = feature.BasicGeometry as LineString;
+                    if (linestr != null)
+                    { // case if powerline consists of one line
+                        // get coordinates list
+                        IList<Coordinate> points = linestr.Coordinates;
+                        IFeature brklineFeature = brklineSet.AddFeature(linestr);
+                        //get associated attributes
+                        DataRow featureData = feature.DataRow;
+                        int id = int.Parse(featureData["PW_ID"].ToString());
+                        int year = int.Parse(featureData["Year"].ToString());
+                        double height = double.Parse(featureData["height"].ToString());
+                        // cycle throw all points in line
+                        for (int i=1; i< points.Count; i++)
+                        {
+                            List<CheckPoint> segmentCheckList = new List<CheckPoint>();
+                            double x1 = points[i - 1].X;
+                            double y1 = points[i - 1].Y;
+                            double x2 = points[i].X;
+                            double y2 = points[i].Y;
+                            segmentCheckList = CalcBrkPoint(x1,y1,x2,y2,distThreshold,u_rasterLayer,v_rasterLayer);
+                            lineCheckList.AddRange(segmentCheckList);
+                        }
+
+                        fullCheckList.AddRange(lineCheckList);
+                        brklineFeature.CopyAttributes(feature);
+                    }
+                    else
+                    {//case if powerline is multiline
+                        MultiLineString multiline = feature.BasicGeometry as MultiLineString;
+                        if ( multiline != null){ 
+                            foreach (IGeometry line in multiline.Geometries)
+                            {
+                                IList<Coordinate> points = line.Coordinates;
+                            }
+                            IFeature brklineFeature = brklineSet.AddFeature(multiline);
+                            MessageBox.Show("Works");
+                        }
+
+
+                    }
+                }
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong");
+            }
+
+        }
+    }
+    public class PathPoint
+    {
+        public double X1;
+        public double Y1;
+        public double X2;
+        public double Y2;
+        public double Distance;
+        public double Uwind;
+        public double Vwind;
+        public int Year;
+    }
+    public class CheckPoint
+    {
+        public double X;
+        public double Y;
+        public bool Ifbroken;
+    }
+
+}
